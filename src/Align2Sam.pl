266c4f0e54d291371ae42b900c5da12d20503f1a
Bernie <florbitous@gmail.com>
2010-07-09 16:40:31 +1000

    Wrapper script for Align2Sam.

---
+++ src/Align2Sam.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Align2Sam;
+
+$| = 1;
+
+die qq(
+Usage:   Align2Sam.pl <ref.fa> <alignment.sai> 
+) if(@ARGV < 2);
+
+my $REFERENCE = shift @ARGV;
+my $ALIGNMENT = shift @ARGV;
+my @SAMFLAGS = ()
+
+Align::align2Sam(@SAMFLAGS, $REFERENCE, $ALIGNMENT);
