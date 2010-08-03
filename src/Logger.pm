03cfff7983ae6a4668441a1fc17f195ea929ec0e
Bernie <florbitous@gmail.com>
2010-08-03 16:45:42 +1000

    Fixed up comments.

--- src/Logger.pm
+++ src/Logger.pm
@@ -8,13 +8,12 @@ sub init_logger
    my ($LOG_FILE) = @_;
 
    # if log file specified
-   # XXX should push this to its own module.
    if(defined $LOG_FILE)
    {
       open (FH, ">$LOG_FILE");
       close (FH);
       # Open the log file and redirect output to it
-      # Not sure if redirecting 
+      # Not sure if redirecting STDERR is a good idea?
       open (STDERR, ">>$LOG_FILE");
       open (STDOUT, ">>$LOG_FILE");
       my $now = localtime time;
